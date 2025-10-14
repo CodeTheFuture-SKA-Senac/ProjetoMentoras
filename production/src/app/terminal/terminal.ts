cbf80eb5e0d38ce4d1c8406ca110ca2cc7ab6839
Andressa Hensel Cardoso <andressa.cardoso@ska.com.br>
2025-10-14 17:46:51 -0300

    Aula 4 - Desenvolvimento

--- production/src/app/terminal/terminal.ts
+++ production/src/app/terminal/terminal.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { ProductionControl } from '../production-control/production-control'
+import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { DialogSelect } from '../dialog-select/dialog-select';
+
+// JSON
+import productionOrders from '../../assets/files/production-orders.json';
 
 @Component({
   selector: 'app-terminal',
-  imports: [ProductionControl],
+  imports: [ProductionControl, MatDialogModule],
   templateUrl: './terminal.html',
   styleUrl: './terminal.scss'
 })
+
 export class Terminal {
 
+  readonly dialog: MatDialog = inject(MatDialog);
+
+  productionOrders: any[] = productionOrders;
+
+  setProductionOrder() {
+    console.log('teste evento de clique')
+    this.openSelectDialog();
+  }
+
+  openSelectDialog(): any {
+    const dialogRef: MatDialogRef<DialogSelect, any> = this.dialog.open(DialogSelect, {
+      width: '950px',
+      panelClass: 'custom-dialog',
+      data: {dialogTitle: 'Teste dialog', optionsList: this.productionOrders}
+    });
+
+    return new Promise((resolve) => {
+      dialogRef.afterClosed().subscribe((result) => {
+        resolve(result);
+      })
+    })
+  }
 }
