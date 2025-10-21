ae56436025325ef2d43df4dd2190951e8867810f
Amanda Moraes Benites <amanda.benites@ska.com.br>
2025-10-21 16:26:42 -0300

    Adicionando lógica das dialogs

--- production/src/app/terminal/terminal.ts
+++ production/src/app/terminal/terminal.ts
@@ -1,10 +1,19 @@
-import { Component, inject, Inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductionControl } from '../production-control/production-control'
 import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { DialogSelect } from '../dialog-select/dialog-select';
 
+
+import { ProductionOrder } from '../../../../common/ProductionOrder'
+import { ProductionStatus } from '../../../../common/ProductionStatus'
+import { OrderColors_e, ProductionStatus_e, ProductionStatusColor_e } from '../../../../common/enums/enum';
+
+// import { MatButtonModule } from '@angular/material/button';
+
+
 // JSON
 import productionOrders from '../../assets/files/production-orders.json';
+import stopTypes from '../../assets/files/stop-types.json'
 
 @Component({
   selector: 'app-terminal',
@@ -17,18 +26,50 @@ export class Terminal {
 
   readonly dialog: MatDialog = inject(MatDialog);
 
-  productionOrders: any[] = productionOrders;
+  productionOrders: ProductionOrder[] = productionOrders as ProductionOrder[];
+  stopTypes: any = stopTypes
 
-  setProductionOrder() {
-    console.log('teste evento de clique')
-    this.openSelectDialog();
+  productionOrder: ProductionOrder = new ProductionOrder();
+  productionStatus: ProductionStatus = new ProductionStatus();
+
+  OrderColors_e: typeof OrderColors_e = OrderColors_e;
+  ProductionStatus_e: typeof ProductionStatus_e = ProductionStatus_e;
+
+  async setProductionOrder(): Promise<void> {
+    const dialogData: Object = {
+      dialogTitle: 'Selecionar ordem de produção',
+      optionsList: this.productionOrders
+    }
+
+    const newProductionOrder: ProductionOrder = await this.openSelectDialog(dialogData);
+    if (!newProductionOrder) return;
+
+    this.productionOrder = newProductionOrder;
+    console.log('ProductionStatus_e :', ProductionStatus_e);
+
+    this.productionStatus = new ProductionStatus(ProductionStatus_e.InProduction, ProductionStatusColor_e.InProduction)
   }
 
-  openSelectDialog(): any {
+  async setStopType(): Promise<void> {
+    const dialogData: Object = {
+      dialogTitle: 'Selecionar motivo de parada',
+      optionsList: this.stopTypes
+    }
+
+    const stopType = await this.openSelectDialog(dialogData);
+    if (!stopType) return;
+
+    this.productionStatus = {
+      color: ProductionStatusColor_e.Stop,
+      status: ProductionStatus_e[stopType.value as keyof typeof ProductionStatus_e]
+    }
+  }
+
+  openSelectDialog(dialogData: any): Promise<ProductionOrder> {
     const dialogRef: MatDialogRef<DialogSelect, any> = this.dialog.open(DialogSelect, {
       width: '950px',
       panelClass: 'custom-dialog',
-      data: {dialogTitle: 'Teste dialog', optionsList: this.productionOrders}
+      data: dialogData
     });
 
     return new Promise((resolve) => {
